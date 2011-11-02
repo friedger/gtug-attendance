96236216915c2c8f70444139da775aa93475256a
Sander Versluys <versluyssander@gmail.com>
2011-11-02 16:51:13 +0100

    add temp spring project for testing

---
+++ Attendance-Web/src/org/brussels/gtug/attendance/HelloController.java
@@ -0,0 +1,24 @@
+package org.brussels.gtug.attendance;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/*")
+public class HelloController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public @ResponseBody
+	String writeString() {
+		return "Congratulations..spring is runninwg !";
+	}
+	
+	@RequestMapping(value = "/thing", method = RequestMethod.GET)
+	public @ResponseBody
+	String writeThing() {
+		return "Congratulations..boeeee is runninwg !";
+	}
+
+}
