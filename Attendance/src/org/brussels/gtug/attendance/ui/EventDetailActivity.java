a06c5d3a52eccef24a3f2bb70bd17c8d24b28e23
Sander Versluys <versluyssander@gmail.com>
2011-11-08 16:17:16 +0100

    show title on details activity

--- Attendance/src/org/brussels/gtug/attendance/ui/EventDetailActivity.java
+++ Attendance/src/org/brussels/gtug/attendance/ui/EventDetailActivity.java
@@ -5,6 +5,7 @@ import org.brussels.gtug.attendance.model.Event;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EventDetailActivity extends Activity {	
 	
@@ -12,22 +13,29 @@ public class EventDetailActivity extends Activity {
 	
 	private Event event;
 	
+	private TextView textName;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event_detail);
+		findAndSetupViews();
 		
 		Bundle extras = getIntent().getExtras();
 		if (extras != null && extras.containsKey(EXTRA_EVENT)) {
 			event = (Event) extras.getSerializable(EXTRA_EVENT);
-			
+			showEvent();
 		} else {
 			finish();
 		}
 	}
 	
+	protected void findAndSetupViews() {
+		textName = (TextView) findViewById(R.id.name);
+	}
+	
 	protected void showEvent() {
-		
+		textName.setText(event.getName());
 	}
 		
 	
